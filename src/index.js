75d56482e7d1ff46b0fb9751d918732638999624
Nicholas Beerbower <nbeerbower@gmail.com>
2023-07-30 16:49:55 -0400

    module export

---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Packet: require('./network/Packet'),
+    Server: require('./network/Server'),
+    Client: require('./network/Client'),
+    Connection: require('./network/Connection'),
+}
